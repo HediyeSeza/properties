bd49c605ea107675e3726c6fec0930ab7e81ba49
sezahedi <sezahedi13@gmail.com>
2022-07-18 00:42:06 +0430

    Add project files.

---
+++ properties/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    public class Employee
+    {
+       int id;
+        string name;
+        int age;
+        public void  setId(int id)
+        {
+            this.id = id;
+        }
+        public int getId()
+        {
+            return this.id;
+        }
+    }
+}
---
+++ properties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em =new Employee();
+            em.setId(2021);
+            Console.WriteLine("your id Employee is :");
+            Console.WriteLine(em.getId());
+            Console.ReadLine();
+        }
+    }
+}
